8d60e62434da93891e7028c9f6e5946ebc11b89d
yinm <yinm.cminor9@gmail.com>
2017-11-07 07:32:36 +0900

    Add class method test

--- rails-4-1-rspec-3-0-01_untested/spec/models/contact_spec.rb
+++ rails-4-1-rspec-3-0-01_untested/spec/models/contact_spec.rb
@@ -52,4 +52,24 @@ describe Contact do
     )
     expect(contact.name).to eq 'John Doe'
   end
+
+  it "returns a sorted array of results that match" do
+    smith = Contact.create(
+      firstname: 'John',
+      lastname: 'Smith',
+      email: 'jsmith@example.com'
+    )
+    jones = Contact.create(
+      firstname: 'Tim',
+      lastname: 'Jones',
+      email: 'tjones@example.com'
+    )
+    johnson = Contact.create(
+      firstname: 'John',
+      lastname: 'Johnson',
+      email: 'jjhonson@example.com'
+    )
+
+    expect(Contact.by_letter('J')).to eq [johnson, jones]
+  end
 end
