c0bce48241680bd993c5eda7dd1b9697fc167b59
yinm <yinm.cminor9@gmail.com>
2017-11-07 07:37:42 +0900

    Add class method test

--- rails-4-1-rspec-3-0-01_untested/spec/models/contact_spec.rb
+++ rails-4-1-rspec-3-0-01_untested/spec/models/contact_spec.rb
@@ -72,4 +72,24 @@ describe Contact do
 
     expect(Contact.by_letter('J')).to eq [johnson, jones]
   end
+
+  it "omits results that do not match" do
+    smith = Contact.create(
+      firstname: 'John',
+      lastname: 'Smith',
+      email: 'jsminth@example.com'
+    )
+    jones = Contact.create(
+      firstname: 'Tim',
+      lastname: 'Jones',
+      email: 'tjones@example.com'
+    )
+    johnson = Contact.create(
+      firstname: 'John',
+      lastname: 'Johnson',
+      email: 'jjohnson@example.com'
+    )
+
+    expect(Contact.by_letter('J')).not_to include smith
+  end
 end
