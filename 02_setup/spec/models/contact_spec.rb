568f66f66330b4c676f827476d5185277e344dd1
yinm <yinm.cminor9@gmail.com>
2017-11-09 08:33:31 +0900

    Add context

--- 02_setup/spec/models/contact_spec.rb
+++ 02_setup/spec/models/contact_spec.rb
@@ -54,43 +54,11 @@ describe Contact do
     expect(contact.name).to eq 'John Doe'
   end
 
-  it "returns a sorted array of results that match" do
-    smith = Contact.create(
-      firstname: 'John',
-      lastname: 'Smith',
-      email: 'jsmith@example.com'
-    )
-    jones = Contact.create(
-      firstname: 'Tim',
-      lastname: 'Jones',
-      email: 'tjones@example.com'
-    )
-    johnson = Contact.create(
-      firstname: 'John',
-      lastname: 'Johnson',
-      email: 'jjohnson@example.com'
-    )
+  describe "filter last name by letter" do
+    context "matching letters" do
+    end
 
-    expect(Contact.by_letter('J')).to eq [johnson, jones]
-  end
-
-  it "omits results that do not match" do
-    smith = Contact.create(
-      firstname: 'John',
-      lastname: 'Smith',
-      email: 'jsmith@example.com'
-    )
-    jones = Contact.create(
-      firstname: 'Tim',
-      lastname: 'Jones',
-      email: 'tjones@example.com'
-    )
-    johnson = Contact.create(
-      firstname: 'John',
-      lastname: 'Johnson',
-      email: 'jjohnson@example.com'
-    )
-
-    expect(Contact.by_letter('J')).not_to include smith
+    context "non-matching letters" do
+    end
   end
 end
