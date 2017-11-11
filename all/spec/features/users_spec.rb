5dec6d3784b686988e548251a0ad23d336b6897b
yinm <yinm.cminor9@gmail.com>
2017-11-11 17:43:34 +0900

    Add helper

--- all/spec/features/users_spec.rb
+++ all/spec/features/users_spec.rb
@@ -3,12 +3,7 @@ require 'rails_helper'
 feature 'User management' do
   scenario 'adds a new user' do
     admin = create(:admin)
-
-    visit root_path
-    click_link 'Log In'
-    fill_in 'Email', with: admin.email
-    fill_in 'Password', with: admin.password
-    click_button 'Log In'
+    sign_in admin
 
     visit root_path
     expect{
